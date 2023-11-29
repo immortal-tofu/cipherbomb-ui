9bd1ea2038228e295a0f83c4f9e526ca42c6ce4c
Clément 'birdy' Danjou <clement@danjou.io>
2023-11-29 23:06:06 +0100

    fix() fix music to play when click

--- src/utils/music.ts
+++ src/utils/music.ts
@@ -1,13 +1,32 @@
 import music from '../assets/music.mp3';
 
+let enabled = false;
+let waitToPlay = false;
+
 const musicAudio = new Audio(music);
 musicAudio.loop = true;
 musicAudio.volume = 0.5;
 
 export const playMusic = () => {
-  void musicAudio.play();
+  if (enabled) {
+    void musicAudio.play();
+  } else {
+    waitToPlay = true;
+  }
 };
 
 export const pauseMusic = () => {
-  void musicAudio.pause();
+  if (enabled) {
+    void musicAudio.pause();
+  } else {
+    waitToPlay = false;
+  }
 };
+
+const enableMusic = () => {
+  enabled = true;
+  if (waitToPlay) void musicAudio.play();
+};
+
+document.addEventListener('click', enableMusic);
+document.addEventListener('tap', enableMusic);
